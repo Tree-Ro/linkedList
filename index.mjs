149a53cbb3464e91d5790013172ab50614867c9f
David Steiner <david1.steiner@gmail.com>
2024-05-18 17:50:18 +0200

    Finished the project

--- index.mjs
+++ index.mjs
@@ -31,8 +31,91 @@ class LinkedList {
     if (newNode === null) return node;
     return this.tail(newNode);
   }
+
+  at(index) {
+    let node = this;
+    for (let i = 0; i < index; ++i) {
+      node = node.nextNode;
+    }
+    return node;
+  }
+
+  pop() {
+    const size = this.size();
+    this.at(size - 2).nextNode = null;
+
+    return this;
+  }
+
+  contains(value) {
+    let node = this;
+    for (let i = 0; i < this.size(); ++i) {
+      if (node.value == value) {
+        return true;
+      }
+      node = node.nextNode;
+    }
+    return false;
+  }
+
+  find(value) {
+    let node = this;
+    for (let i = 0; i < this.size(); ++i) {
+      if (node.value == value) {
+        return i;
+      }
+      node = node.nextNode;
+    }
+    return null;
+  }
+
+  toString() {
+    let currentNode = this;
+    let stringified = '';
+
+    while (currentNode) {
+      stringified += `( ${currentNode.value} ) -> `;
+      currentNode = currentNode.nextNode;
+    }
+
+    // Remove the last '->' from the string
+    stringified = stringified.slice(0, -4);
+
+    stringified += `-> null`;
+
+    return stringified;
+  }
+
+  insertAt(value, index) {
+    if (index <= 0) return "Can't insert at head";
+    let node = this;
+    let lastNode = node;
+    for (let i = -1; i < index; ++i) {
+      if (i === index - 1) {
+        const addition = new Node(value, node);
+        lastNode.nextNode = addition;
+      }
+      lastNode = node;
+      node = node.nextNode;
+    }
+    return this;
+  }
+
+  removeAt(index) {
+    const pre = this.at(index - 1);
+    pre.nextNode = pre.nextNode.nextNode;
+    return this;
+  }
 }
 
 console.log(
-  new LinkedList('Old Head').prepend('New Head').append('Node').append('2Node')
+  new LinkedList('Old Head')
+    .prepend('New Head')
+    .append('Node')
+    .append('2Node')
+    .at(0)
+    .pop()
+    .append('3rdNode')
+    .insertAt('insertAt Node test', 2)
+    .removeAt()
 );
